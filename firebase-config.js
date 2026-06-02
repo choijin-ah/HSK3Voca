aba047d2faca9a7b3a339b79603a42bc0d98150c
최진아 <naively3@gmail.com>
2026-06-02 12:36:56 +0900

    Configure Firebase app settings

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,11 @@
-// Firebase Console > Project settings > Your apps > Web app config 값을 붙여 넣으세요.
-// 이 값들은 비밀번호가 아니며, 실제 접근 제한은 Firestore 보안 규칙이 담당합니다.
+// Firebase Console > Project settings > Your apps > Web app config.
+// These values identify this Firebase app. Data access is controlled by Firestore rules.
 window.HSK3_FIREBASE_CONFIG = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyBYzj6RQ501d3e9toCGfW0syazh2cBajZQ",
+  authDomain: "hsk3-3a543.firebaseapp.com",
+  projectId: "hsk3-3a543",
+  storageBucket: "hsk3-3a543.firebasestorage.app",
+  messagingSenderId: "392710115710",
+  appId: "1:392710115710:web:581b65591a1789eef2a32f",
+  measurementId: "G-58BN6BK24C"
 };
